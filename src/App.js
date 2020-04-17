b966b00fa839a1dc765aecc503f25082fa5f2dba
Nicolas Aguirre <in-aguirre@hotmail.com>
2020-04-17 14:17:51 -0500

    saving progress

--- src/App.js
+++ src/App.js
@@ -3,8 +3,35 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <h1>INAGUIRRE</h1>
+    <div className="app-container">
+      <div className="top-container">
+        <h1>inaguirre</h1>
+        <h3>Ignacio Nicolas Aguirre</h3>
+      </div>
+      <div className="skills-container">
+        <div className="skill-box">
+          <h4>Entrepreneur</h4>
+          <div className="skill-info-container">
+            <h2>Snowball Financial Education LLC</h2>
+            <p>
+              Learn to manage your financial life with real-life based financial
+              simulations
+            </p>
+            <button
+              onClick={() => console.log("App.js 17 | open snowball")}
+              className="snowball-button-link"
+            >
+              Check it out!
+            </button>
+          </div>
+        </div>
+        <div className="skill-box">
+          <h4>Software Developer</h4>
+        </div>
+        <div className="skill-box">
+          <h4>Writer</h4>
+        </div>
+      </div>
     </div>
   );
 };
